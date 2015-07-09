9f5411054e50e09d889a658e0097210263653be4
Lee Fallat <me@leefallat.ca>
2015-07-09 10:32:57 -0400

    Clean up and optimal message sending size.

--- mailman.py
+++ mailman.py
@@ -6,10 +6,13 @@ import time
 import sys
 
 _BOT_SUFFIX = "_mailman"
+_COMMAND_PREFIX = ":"
+_FREQUENCY = 2
 
 class B64OIRC(irc.bot.SingleServerIRCBot):
     def __init__(self, owner, server, channel, port):
         global _BOT_SUFFIX
+        
         self.owner = owner
         self.channel = channel
         self.nickname = owner+_BOT_SUFFIX
@@ -18,9 +21,11 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
         self.accept = False
         self.locked = False
         self.sink = ""
-        self.interpret = True
-        self._COMMANDS = ["send", "receive", "cancel"]
-        irc.bot.SingleServerIRCBot.__init__(self, [(server,port)], self.nickname, self.nickname)
+        self.interpret = True # Set to false if you want no commands interpreted
+        self.commands = ["send", "receive", "cancel"]
+        
+        super().__init__([(server,port)], self.nickname, self.nickname)
+        self.connection.set_rate_limit(2)
 
     def on_welcome(self, context, event):
         context.join(self.channel)
@@ -36,23 +41,19 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
 
     def do(self, context, event):
         if self.accept:
-            self.eat(context, event)
-        else:
+            self.consume(context, event)
+        elif self.interpret:
             self.understand(context, event)
 
-    def is_owner(self, context, event):
-        user = event.source.split("!")[0]
-        if(user != self.owner):
-            return False
-        return True
+    def owner(self, user):
+        return user == self.owner
 
     def understand(self, context, event):
-        if not self.interpret:
-            return
-        text = event.arguments[0]
-        chunks = text.split(" ")
-        for command in self._COMMANDS:
-            if chunks[0] == ":"+command:
+        global _COMMAND_PREFIX
+        
+        chunks = self.get_args(event)
+        for command in self.commands:
+            if chunks[0] == _COMMAND_PREFIX+command:
                 print(command)
                 getattr(self, command)(context,event)
 
@@ -61,6 +62,12 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
 
     def get_transmitter(self, event):
         return event.source.split("!")[0]
+
+    def cancel(self, context, event):
+        context.privmsg(self.channel, "Cancelled transaction.")
+        self.locked = False
+        self.target = {}
+        self.source = {}
 
     def send(self, context, event):
         global _BOT_SUFFIX
@@ -73,56 +80,53 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
             return
 
         parameters = {'user': chunks[1], 'filename': chunks[2]}
-        
-        if transmitter == self.owner:
+
+        # The owner is sending data and the bot acknowledges.
+        if owner(transmitter):
             self.target['user'] = parameters['user']
             self.target['filename'] = parameters['filename']
             context.privmsg(self.channel, "Target acquired.")
 
-        if parameters['user'] == self.owner:
+        # Another owner is sending data to our bot, and waits for approval.
+        if owner(parameters['user']):
             self.source['user'] = transmitter+_BOT_SUFFIX
             self.source['filename'] = parameters['filename']
             context.privmsg(self.channel, "Awaiting receive.")
         
         context.privmsg(self.channel, "Locked.")
         self.locked = True
-
-    def cancel(self, context, event):
-        context.privmsg(self.channel, "Cancelled transaction.")
-        self.locked = False
-        self.target = {}
-        self.source = {}
         
     def receive(self, context, event):
+        # Ensure we have "locked in" to a target or source.
         if not self.locked:
             context.privmsg(self.channel, "Not locked in.")
             return
 
         transmitter = self.get_transmitter(event)
 
+        # If there is no target, it means we are receiving.
         if transmitter == self.owner and len(self.target) == 0:
             context.privmsg(self.channel, "Owner has accepted.")
             self.accept = True
             return
-        
+
+        # If there is a target, they have accepted and we begin sending.
         if transmitter == self.target['user']:
             os.call("gzip "+self.target['filename']+"; base64 "+self.target['filename']+".gz | tr -d '\n' > out", shell=True)
             with open("out","r") as fh:
-                data = fh.read()
-            
-            index = 0
-            target = self.channel
-            msg_length = 510-(len(target)+50)
-            while index < len(data):
-                context.privmsg(target, data[index:index+msg_length])
-                index += msg_length
-                time.sleep(1)
+                # In the IRC package, client.py sends PRIVMSG %s :%s.
+                # We take that, and calculate our max size of the datagram.
+                msg_length = (512 - len("PRIVMSG  :")) - len(self.channel);
+                data = fh.read(msg_length)
+                while data != '':
+                    context.privmsg(self.channel, data)
+                    data = fh.read(msg_length)
 
             context.privmsg(self.channel, ":eof")
             self.target = {}
             self.locked = False
  
-    def eat(self, context, event):
+    def consume(self, context, event):
         transmitter = self.get_transmitter(event)
 
         if transmitter != self.source['user']:
@@ -133,10 +137,12 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
 
         if(parameters['command'] == ":eof"):
             context.privmsg(self.channel, "EOF encountered. Writing file.")
+            
             if(len(self.sink) > 0):
                 with open("in", "w") as fh:
                     fh.write(self.sink)
                 os.call("base64 -d in > "+self.source['filename']+".gz; gunzip "+self.source['filename']+".gz;", shell=True)
+                
                 context.privmsg(self.channel, ":complete")
             self.sink = ""
             self.source = {}
@@ -148,10 +154,11 @@ class B64OIRC(irc.bot.SingleServerIRCBot):
 
 def main():
     if(len(sys.argv) != 4):
-        print("Usage: python howard.py username server channel\nNote: bash is a bitch, remember # is comment in bash.")
-    your_irc_username = sys.argv[1]
+        print("Usage: python mailman.py owner server channel\nNote: bash is a bitch, remember # is comment in bash.")
+        return
+    owner = sys.argv[1]
     server = sys.argv[2]
     channel = sys.argv[3]
-    B64OIRC(your_irc_username, server, channel, 6667).start()
+    B64OIRC(owner, server, channel, 6667).start()
 
 main()
